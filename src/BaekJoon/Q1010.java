6295683c5f74a7d44e2efa78af5bb9a920db1203
DK <dongwook5478@gmail.com>
2023-05-16 16:09:28 +0900

    add Q1010

---
+++ src/BaekJoon/Q1010.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Q1010 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int east, west;
+        //east is also the maximum number of bridge
+
+        east=sc.nextInt();
+        west=sc.nextInt();
+
+
+        System.out.println(east+" "+west);
+
+    }
+}
