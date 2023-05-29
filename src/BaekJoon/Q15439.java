f4111cffac6a4eb4b9049f5cbed52bd321af6605
DK <dongwook5478@gmail.com>
2023-05-29 21:14:05 +0900

    complited Q15439

--- src/BaekJoon/Q15439.java
+++ src/BaekJoon/Q15439.java
@@ -1,4 +1,11 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class Q15439 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int first=sc.nextInt();
+        System.out.println(first*(first-1));
+    }
 }
