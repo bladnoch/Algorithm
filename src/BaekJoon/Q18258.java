991ad63cd0318b7541a612a68b140a63a6eba12a
DK <dongwook5478@gmail.com>
2023-05-30 15:57:21 +0900

    update Q18258

--- src/BaekJoon/Q18258.java
+++ src/BaekJoon/Q18258.java
@@ -6,7 +6,9 @@ import java.util.Scanner;
 
 public class Q18258 {
     private static Scanner sc=new Scanner(System.in);
-    private List<Integer>num=new ArrayList<>();
+    private static List<Integer>tempNum=new ArrayList<>();
+    private static List<Integer>num=new ArrayList<>();
+
     public static void main(String[] args){
 
         int loop=sc.nextInt();
@@ -14,14 +16,59 @@ public class Q18258 {
 
     }
     public static void looper(int loop){
-        String check;
+        checker(sc.nextLine());
         for (int i=0;i<loop;i++){
-            check=sc.next();
-            if (check.equals(push))
+
         }
     }
-    public static int push(){
+    public static void push(String item){
+        String[] ogItem=item.split(" ");
+        tempNum.add(Integer.parseInt(ogItem[1]));
+        num.add(Integer.parseInt(ogItem[1]));
+    }
+    public static void front(){
+        if(tempNum.isEmpty()){
+            tempNum.add(-1);
+        } else {
+            num.add(tempNum.get(0));
+        }
 
-        return 0;
+    }
+    public static void back(){
+        if(tempNum.isEmpty()){
+            tempNum.add(-1);
+        }
+        else{
+            num.add(tempNum.get(tempNum.size()-1));
+        }
+    }
+    public static void pop(){
+        if(num.isEmpty()){
+            num.add(-1);
+        } else {
+            num.
+        }
+    }
+    public static void mySize(){
+
+    }
+    public static void myEmpty(){
+
+    }
+
+    public static void checker(String item){
+        if (item.equals("front")){
+
+        }else if (item.equals("back")){
+
+        } else if (item.equals("pop")) {
+
+        } else if (item.equals("size")) {
+
+        } else if(item.equals("empty")){
+
+        } else {
+
+        }
     }
 }
