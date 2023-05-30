b437dcbe508fc0c74fee2fbdb8cc8cbf6f0e550d
DK <dongwook5478@gmail.com>
2023-05-30 15:34:14 +0900

    Q18258

---
+++ src/BaekJoon/Q18258.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q18258 {
+    private static Scanner sc=new Scanner(System.in);
+    private List<Integer>num=new ArrayList<>();
+    public static void main(String[] args){
+
+        int loop=sc.nextInt();
+        looper(loop);
+
+    }
+    public static void looper(int loop){
+        String check;
+        for (int i=0;i<loop;i++){
+            check=sc.next();
+            if (check.equals(push))
+        }
+    }
+    public static int push(){
+
+        return 0;
+    }
+}
